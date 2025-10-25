00eea8bef03c2d96534cddb4831f48ac04428419
Deepthi-M555 <mdeepthi555@gmail.com>
2025-10-25 16:04:50 +0530

    feat: Configure MongoDB Atlas for deployment and add MongoStore sessions

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError.js');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
@@ -19,7 +20,8 @@ const listingsRouter = require('./routes/listing.js');
 const reviewRouter = require('./routes/review.js');
 const userRouter = require('./routes/user.js');
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
     .then(() =>{
@@ -28,7 +30,7 @@ main()
     .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -38,8 +40,20 @@ app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: "mysupersecretcode"
+    },
+    touchAfter: 24*3600,
+});
+
+store.on("error", () =>{
+    console.log("ERROR IN MONGOSESSION STORE", err)
+});
 
 const sessionOptions = {
+    store,
     secret: 'mysupersecretcode',
     resave: false,
     saveUninitialized: true,
@@ -50,9 +64,9 @@ const sessionOptions = {
     }
 };
 
-app.get("/", (req, res)=>{
-    res.send("Hello, Welcome to Wanderlust!");
-});
+// app.get("/", (req, res)=>{
+//     res.send("Hello, Welcome to Wanderlust!");
+// });
 
 app.use(session(sessionOptions));
 app.use(flash());
